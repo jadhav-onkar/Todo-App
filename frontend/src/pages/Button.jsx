1c45d62fcce69a58179ae871642d0d6a33f0afa8
Gani0230 <ganeshjadhav7478@gmail.com>
2025-06-18 20:37:54 +0530

    do some styling work

--- frontend/src/pages/Button.jsx
+++ frontend/src/pages/Button.jsx
@@ -5,8 +5,9 @@ import { useNavigate } from "react-router-dom"
 export default function Button(){
     const navigate = useNavigate()
     return(
-        <div>
-            <button onClick={()=>navigate('/signin')}>Sign in</button>
+        <div className="h-screen flex flex-col justify-center items-center">
+            <h1 className="text-4xl mb-5 font-bold">Todo App</h1>
+            <button className="shadow-black shadow-2xl text-2xl p-5 rounded-3xl bg-black text-white hover:text-white font-Ubuntu" onClick={()=>navigate('/signin')}>Sign in</button>
         </div>
     )
 }
--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -30,15 +30,18 @@ export default function Signin(){
      }
 
     return(
-        <div>
-            <p>Email</p>
-            <input onChange={(e)=>setEmail(e.target.value)} type="text" placeholder="abc@gmail.com" />
+        <div className="h-screen flex justify-center bg-gray-100 items-center">
+            <div className="border border-gray-300 bg-white p-10 flex flex-col rounded-3xl">
+                <h1 className="text-3xl font-Ubuntu font-bold mx-auto mb-5">Sign In</h1>
+                <p className="font-bold mb-2">Email</p>
+                <input className="border border-gray-400 p-2 mb-5 rounded-lg" onChange={(e)=>setEmail(e.target.value)} type="text" placeholder="abc@gmail.com" />
 
-            <p>Password</p>
-            <input onChange={(e)=>setPassword(e.target.value)} type="text" placeholder="test123" />
+                <p className="font-bold mb-2">Password</p>
+                <input className="border border-gray-400 p-2 mb-4 rounded-lg" onChange={(e)=>setPassword(e.target.value)} type="text" placeholder="test123" />
 
-            <button onClick={userSignin}>Sign in</button>
-            <p>if not have account? <button onClick={()=>navigate('/signup')}>sign up</button></p>
+                <button className="bg-black hover:bg-gray-600 text-white p-2 rounded-lg" onClick={userSignin}>Sign in</button>
+                <p className="mt-2">If not have account? <button className="hover:text-gray-500 underline" onClick={()=>navigate('/signup')}>sign up</button></p>
+            </div>
         </div>
     )
 }

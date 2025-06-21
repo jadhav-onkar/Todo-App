ad5694ac28d75f895c259cb031168a414a1ce880
Gani0230 <ganeshjadhav7478@gmail.com>
2025-06-21 16:21:45 +0530

    add forms in signup and sign in page

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -31,17 +31,21 @@ export default function Signin(){
      }
 
     return(
-        <div className="min-h-screen flex justify-center bg-[#101323] p-5  items-center">
+        <div className="min-h-screen flex justify-center bg-[#101323] p-5 items-center">
             <div className="bg-[#171c33] p-10 mx-2 sm:mx-0 flex flex-col rounded-3xl w-[350px] sm:w-[400px]">
                 <h1 className="text-3xl font-Ubuntu font-bold text-gray-400 mx-auto mb-5">Sign In</h1>
-                <p className="font-bold mb-2 text-gray-400">Email</p>
-                <input className="border border-gray-400 p-2 mb-5 rounded-lg text-gray-400" onChange={(e)=>setEmail(e.target.value)} type="text" placeholder="abc@gmail.com" />
 
-                <p className="font-bold mb-2 text-gray-400">Password</p>
-                <input className="border border-gray-400 p-2 mb-5 rounded-lg  text-gray-400" onChange={(e)=>setPassword(e.target.value)} type="text" placeholder="test123" />
+                <form onSubmit={userSignin} className="flex flex-col">
+                    <p className="font-bold mb-2 text-gray-400">Email</p>
+                    <input className="border border-gray-400 p-2 mb-5 rounded-lg text-gray-400" onChange={(e) => setEmail(e.target.value)} type="email" placeholder="abc@gmail.com" required/>
 
-                <button className="bg-[#607AFB] hover:bg-gray-600 text-white p-2 rounded-lg" onClick={userSignin}>Sign in</button>
-                <p className="mt-3 mx-auto text-gray-400">If not have account? <button className="hover:text-gray-500 underline" onClick={()=>navigate('/signup')}>sign up</button></p>
+                    <p className="font-bold mb-2 text-gray-400">Password</p>
+                    <input className="border border-gray-400 p-2 mb-5 rounded-lg text-gray-400" onChange={(e) => setPassword(e.target.value)} type="password" placeholder="••••••••" required/>
+
+                    <button type="submit" className="bg-[#607AFB] hover:bg-gray-600 text-white p-2 rounded-lg">Sign in</button>
+                </form>
+
+                <p className="mt-3 mx-auto text-gray-400">If not have account?{' '}<button className="hover:text-gray-500 underline" onClick={() => navigate('/signup')}>Sign up</button></p>
             </div>
         </div>
     )
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -32,20 +32,23 @@ export default function Signup(){
 
     return(
         <div className="min-h-screen flex justify-center bg-[#101323] p-5 items-center">
-            <div  className="bg-[#171c33] mx-2 sm:mx-0 p-10 flex flex-col rounded-3xl w-[350px] sm:w-[400px]">
+            <div className="bg-[#171c33] mx-2 sm:mx-0 p-10 flex flex-col rounded-3xl w-[350px] sm:w-[400px]">
                 <h1 className="text-3xl font-Ubuntu font-bold text-gray-400 mx-auto mb-5">Sign Up</h1>
 
-                <p className="font-bold mb-2 text-gray-400">Username</p>
-                <input className="border border-gray-400 p-2 mb-5 rounded-lg text-gray-400" onChange={(e)=>setUsername(e.target.value)} type="text" placeholder="username" />
+                <form onSubmit={userSignUp} className="flex flex-col">
+                    <p className="font-bold mb-2 text-gray-400">Username</p>
+                    <input className="border border-gray-400 p-2 mb-5 rounded-lg text-gray-400" onChange={(e) => setUsername(e.target.value)} type="text" placeholder="username" required/>
 
-                <p className="font-bold mb-2 text-gray-400">Email</p>
-                <input className="border border-gray-400 p-2 mb-5 rounded-lg text-gray-400" onChange={(e)=>setEmail(e.target.value)} type="text" placeholder="abc@gmail.com" />
+                    <p className="font-bold mb-2 text-gray-400">Email</p>
+                    <input className="border border-gray-400 p-2 mb-5 rounded-lg text-gray-400" onChange={(e) => setEmail(e.target.value)} type="email" placeholder="abc@gmail.com" required/>
 
-                <p className="font-bold mb-2 text-gray-400">Password</p>
-                <input className="border border-gray-400 p-2 mb-5 rounded-lg text-gray-400" onChange={(e)=>setPassword(e.target.value)} type="text" placeholder="pass123" /> 
+                    <p className="font-bold mb-2 text-gray-400">Password</p>
+                    <input className="border border-gray-400 p-2 mb-5 rounded-lg text-gray-400" onChange={(e) => setPassword(e.target.value)} type="password" placeholder="••••••••" required/>
 
-                <button className="bg-[#607AFB] hover:bg-gray-600 text-white p-2 rounded-lg" onClick={userSignUp}>Sign up</button>
-                <p className="mt-3 mx-auto text-gray-400">if already have account? <button className="hover:text-gray-500 underline" onClick={()=>navigate('/signin')}>sign in</button></p>
+                    <button type="submit" className="bg-[#607AFB] hover:bg-gray-600 text-white p-2 rounded-lg">Sign up</button>
+                </form>
+
+                <p className="mt-3 mx-auto text-gray-400">If already have an account? <button className="hover:text-gray-500 underline"onClick={() => navigate('/signin')}>Sign in</button> </p>
             </div>
         </div>
     )
